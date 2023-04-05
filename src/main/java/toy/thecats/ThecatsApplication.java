c6d6737e3ca22845703292227d57c72e6e542af4
Seonho Kim <sunhowold@naver.com>
2023-04-05 18:21:08 +0900

    initial commit

---
+++ src/main/java/toy/thecats/ThecatsApplication.java
@@ -0,0 +1,13 @@
+package toy.thecats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThecatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThecatsApplication.class, args);
+	}
+
+}
