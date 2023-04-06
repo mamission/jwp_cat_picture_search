4c3c2cb3fff19e31f2990db609f1a539a5657092
ysk <kim206gh@naver.com>
2023-04-06 14:28:06 +0900

    chore : 주석 삭제

--- src/main/java/com/ys/cat_picture/cat_breed/repository/CatBreedRepository.java
+++ src/main/java/com/ys/cat_picture/cat_breed/repository/CatBreedRepository.java
@@ -10,9 +10,8 @@ import com.ys.cat_picture.cat_breed.domain.CatBreed;
 
 public interface CatBreedRepository extends JpaRepository<CatBreed, String> {
 
-	// @Query(nativeQuery = true, value = "select exists (select 1 from cat_breeds)")
 	@Query(value = "select exists (select 1 from CatBreed c)")
-	Boolean existsData();
+	boolean existsData();
 
 	Optional<CatBreed> findByExternalId(@Param("externalId")String externalId);
 
