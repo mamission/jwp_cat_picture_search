8962cd4e3e2a4c9e9f68f3c20d73b2fedb31ce79
dlxortmd987 <dlxortmd987@gmail.com>
2023-04-06 21:20:25 +0900

    Feat: 랜덤으로 고양이 사진 가져오는 컨트롤러 코드 구현

---
+++ src/main/java/com/example/catpicture/web/CatPictureApi.java
@@ -0,0 +1,24 @@
+package com.example.catpicture.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.catpicture.domain.CatPictureService;
+import com.example.catpicture.domain.dto.GetRandomCatsResponse;
+
+@RestController
+@RequestMapping("/cats")
+public class CatPictureApi {
+
+	private final CatPictureService catPictureService;
+
+	public CatPictureApi(CatPictureService catPictureService) {
+		this.catPictureService = catPictureService;
+	}
+
+	@GetMapping("/random50")
+	public GetRandomCatsResponse getRandomCats() {
+		return catPictureService.getRandom(50);
+	}
+}
