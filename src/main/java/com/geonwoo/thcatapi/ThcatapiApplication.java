8d1f4034c2f13efac7e889997271a25ee4e30537
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-04-07 03:01:16 +0900

    project init

---
+++ src/main/java/com/geonwoo/thcatapi/ThcatapiApplication.java
@@ -0,0 +1,13 @@
+package com.geonwoo.thcatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThcatapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThcatapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geonwoo/thcatapi/ThcatapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geonwoo.thcatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThcatapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
