db189c4062f1f75c2ed023969340f835454704cd
dlxortmd987 <dlxortmd987@gmail.com>
2023-04-07 19:20:02 +0900

    Feat(repository): 종 이름으로 사진 가져오는 로직 구현

--- src/main/java/com/example/catpicture/domain/CatPictureRepository.java
+++ src/main/java/com/example/catpicture/domain/CatPictureRepository.java
@@ -1,8 +1,18 @@
 package com.example.catpicture.domain;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.catpicture.domain.entity.CatPicture;
 
 public interface CatPictureRepository extends JpaRepository<CatPicture, Long> {
+
+	@Query("""
+			select cp
+			from CatPicture cp
+			where cp.breedDetails.name = :q
+		""")
+	List<CatPicture> findAllByName(String q);
 }
