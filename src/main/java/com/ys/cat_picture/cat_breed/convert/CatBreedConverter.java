3d8fc0569ccf992b6b09bd7315593cd1f5c55cda
ysk <kim206gh@naver.com>
2023-04-07 17:24:00 +0900

    refactor : Breed에서 필요한 데이터만 추출하도록 수정

--- src/main/java/com/ys/cat_picture/cat_breed/convert/CatBreedConverter.java
+++ src/main/java/com/ys/cat_picture/cat_breed/convert/CatBreedConverter.java
@@ -3,7 +3,6 @@ package com.ys.cat_picture.cat_breed.convert;
 import java.util.Optional;
 
 import com.ys.cat_picture.cat_breed.domain.CatBreed;
-import com.ys.cat_picture.cat_breed.domain.Weight;
 import com.ys.cat_picture.infra.client.response.CatOneResponse;
 
 import lombok.AccessLevel;
@@ -18,31 +17,10 @@ public class CatBreedConverter {
 		}
 
 		return Optional.of(CatBreed.builder()
-			.weight(new Weight(response.weight().imperial(), response.weight().metric()))
 			.externalId(response.id())
 			.name(response.name())
-			.cfaUrl(response.cfaUrl())
 			.temperament(response.temperament())
 			.origin(response.origin())
-			.countryCodes(response.countryCodes())
-			.countryCode(response.countryCode())
-			.description(response.description())
-			.lifeSpan(response.lifeSpan())
-			.adaptability(response.adaptability())
-			.affectionLevel(response.affectionLevel())
-			.childFriendly(response.childFriendly())
-			.catFriendly(response.catFriendly())
-			.dogFriendly(response.dogFriendly())
-			.energyLevel(response.energyLevel())
-			.grooming(response.grooming())
-			.healthIssues(response.healthIssues())
-			.intelligence(response.intelligence())
-			.sheddingLevel(response.sheddingLevel())
-			.socialNeeds(response.socialNeeds())
-			.strangerFriendly(response.strangerFriendly())
-			.vocalisation(response.vocalisation())
-			.hypoallergenic(response.hypoallergenic())
-			.referenceImageId(response.referenceImageId())
 			.build());
 	}
 }
--- src/main/java/com/ys/cat_picture/loader/DataLoader.java
+++ src/main/java/com/ys/cat_picture/loader/DataLoader.java
@@ -46,59 +46,17 @@ public class DataLoader implements CommandLineRunner {
 				insert
 				into
 				        cat_breeds
-				        (adaptability, affection_level, alt_names, bidability, cat_friendly, cfa_url, 
-				        child_friendly, country_code, country_codes, description, dog_friendly, energy_level, 
-				        experimental, external_id, grooming, hairless, health_issues, hypoallergenic, indoor, 
-				        intelligence, lap, life_span, name, natural_cat, origin, rare, reference_image_id, rex, 
-				        shedding_level, short_legs, social_needs, stranger_friendly, suppressed_tail, temperament, 
-				        vcahospitals_url, vetstreet_url, vocalisation, imperial, metric, wikipedia_url)
+				        (external_id, name, temperament, origin)
 				    values
-				        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+						(?, ?, ?, ?)
 				""", new BatchPreparedStatementSetter() {
 				@Override
 				public void setValues(PreparedStatement ps, int i) throws SQLException {
 					BreedResponse breedResponse = breeds.get(i);
-					BreedResponse.Weight weight = breedResponse.weight();
-					ps.setInt(1, breedResponse.adaptability());
-					ps.setInt(2, breedResponse.affectionLevel());
-					ps.setString(3, breedResponse.altNames());
-					ps.setInt(4, breedResponse.childFriendly());
-					ps.setInt(5, breedResponse.dogFriendly());
-					ps.setString(6, breedResponse.cfaUrl());
-					ps.setInt(7, breedResponse.childFriendly());
-					ps.setString(8, breedResponse.countryCode());
-					ps.setString(9, breedResponse.countryCodes());
-					ps.setString(10, breedResponse.description());
-					ps.setInt(11, breedResponse.dogFriendly());
-					ps.setInt(12, breedResponse.energyLevel());
-					ps.setInt(13, breedResponse.experimental());
-					ps.setString(14, breedResponse.id());
-					ps.setInt(15, breedResponse.grooming());
-					ps.setInt(16, breedResponse.hairless());
-					ps.setInt(17, breedResponse.healthIssues());
-					ps.setInt(18, breedResponse.hypoallergenic());
-					ps.setInt(19, breedResponse.indoor());
-					ps.setInt(20, breedResponse.intelligence());
-					ps.setInt(21, breedResponse.lap());
-					ps.setString(22, breedResponse.lifeSpan());
-					ps.setString(23, breedResponse.name());
-					ps.setInt(24, breedResponse.naturalCat());
-					ps.setString(25, breedResponse.origin());
-					ps.setInt(26, breedResponse.rare());
-					ps.setString(27, breedResponse.referenceImageId());
-					ps.setInt(28, breedResponse.rex());
-					ps.setInt(29, breedResponse.sheddingLevel());
-					ps.setInt(30, breedResponse.shortLegs());
-					ps.setInt(31, breedResponse.socialNeeds());
-					ps.setInt(32, breedResponse.strangerFriendly());
-					ps.setInt(33, breedResponse.suppressedTail());
-					ps.setString(34, breedResponse.temperament());
-					ps.setString(35, breedResponse.vcahospitalsUrl());
-					ps.setString(36, breedResponse.vetstreetUrl());
-					ps.setInt(37, breedResponse.vocalisation());
-					ps.setString(38, weight.imperial());
-					ps.setString(39, weight.metric());
-					ps.setString(40, breedResponse.wikipediaUrl());
+					ps.setInt(1, breedResponse.experimental());
+					ps.setString(2, breedResponse.name());
+					ps.setString(3, breedResponse.temperament());
+					ps.setString(4, breedResponse.origin());
 				}
 
 				@Override
