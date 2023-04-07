e0279ffc089195e9e4b4697a501bd96ca6e22075
김창규 <kimc980106@naver.com>
2023-04-07 17:58:51 +0900

    Feat : TheCat 서비스 레이어 추가

---
+++ src/main/java/com/example/catpicturesearch/application/service/TheCatService.java
@@ -0,0 +1,40 @@
+package com.example.catpicturesearch.application.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.catpicturesearch.application.domain.CatBreed;
+import com.example.catpicturesearch.application.dto.CatImageDetailResponse;
+import com.example.catpicturesearch.application.dto.CatImageResponse;
+import com.example.catpicturesearch.application.mapper.TheCatMapper;
+import com.example.catpicturesearch.application.repository.CatBreedRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TheCatService {
+
+	private final CatBreedRepository catBreedRepository;
+
+	@Transactional(readOnly = true)
+	public List<CatImageResponse> getRandom50() {
+		List<CatBreed> catBreeds = catBreedRepository.findAllOrderByRandom(50);
+		return TheCatMapper.toCatImageResponses(catBreeds);
+	}
+
+	@Transactional(readOnly = true)
+	public List<CatImageResponse> getCatImagesByBreedId(String breedId) {
+		List<CatBreed> catBreeds = catBreedRepository.findAllByBreedId(breedId);
+		return TheCatMapper.toCatImageResponses(catBreeds);
+	}
+
+	@Transactional(readOnly = true)
+	public CatImageDetailResponse getCatImagesByCatImageId(String catImageId) {
+		CatBreed catBreed = catBreedRepository.findAllByCatImageId(catImageId);
+		return TheCatMapper.toCatImageDetailResponse(catBreed);
+	}
+
+}
