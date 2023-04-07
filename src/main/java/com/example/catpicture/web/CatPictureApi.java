e4ef01995e2b12723202c19049d5e7c2f749877f
dlxortmd987 <dlxortmd987@gmail.com>
2023-04-07 19:20:21 +0900

    Feat(api): 종 이름으로 사진 가져오는 로직 구현

--- src/main/java/com/example/catpicture/web/CatPictureApi.java
+++ src/main/java/com/example/catpicture/web/CatPictureApi.java
@@ -2,9 +2,11 @@ package com.example.catpicture.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.catpicture.domain.CatPictureService;
+import com.example.catpicture.domain.dto.GetCatsByBreedResponse;
 import com.example.catpicture.domain.dto.GetRandomCatsResponse;
 
 @RestController
@@ -20,5 +22,10 @@ public class CatPictureApi {
 	@GetMapping("/random50")
 	public GetRandomCatsResponse getRandomCats() {
 		return catPictureService.getRandom(50);
+	}
+
+	@GetMapping("/cats/search")
+	public GetCatsByBreedResponse getCatsByBreed(@RequestParam String q) {
+		return catPictureService.getByBreed(q);
 	}
 }
