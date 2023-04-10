8f5a8503991e3a9219944ce5fb459361e779df42
yewon.seo <dodnjs1241@naver.com>
2023-04-10 14:05:24 +0900

    환경설정

---
+++ src/main/java/com/cat/mission/CatApplication.java
@@ -0,0 +1,15 @@
+package com.cat.mission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class CatApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CatApplication.class, args);
+  }
+
+}
