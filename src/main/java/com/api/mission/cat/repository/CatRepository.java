0bf512f632fed06f9cb1cd234c41749c6a80a824
yewon.seo <dodnjs1241@naver.com>
2023-04-12 22:15:29 +0900

    refactor: id로 가져오는 리턴타입 optional로 변경

--- src/main/java/com/api/mission/cat/repository/CatRepository.java
+++ src/main/java/com/api/mission/cat/repository/CatRepository.java
@@ -5,6 +5,7 @@ import com.api.mission.cat.dto.CatSimpleResDto;
 import com.api.mission.cat.entity.Cat;
 import com.api.mission.cat.repository.querydsl.CatRepositoryQuerydsl;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -24,5 +25,5 @@ public interface CatRepository extends JpaRepository<Cat, Long>,
       + "c.breeds.name, c.displayId, c.image.url, c.image.width, c.image.height, c.breeds.temperament, c.breeds.origin) "
       + "from Cat c "
       + "where c.displayId = :id")
-  CatDetailResDto getByDisplayId(@Param("id") String id);
+  Optional<CatDetailResDto> getByDisplayId(@Param("id") String id);
 }
--- src/main/java/com/api/mission/cat/service/CatService.java
+++ src/main/java/com/api/mission/cat/service/CatService.java
@@ -3,6 +3,8 @@ package com.api.mission.cat.service;
 import com.api.mission.cat.dto.CatDetailResDto;
 import com.api.mission.cat.dto.CatSimpleResDto;
 import com.api.mission.cat.repository.CatRepository;
+import com.api.mission.global.error.ErrorCode;
+import com.api.mission.infra.catApi.exception.FailedToLoadApiResException;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,7 +30,8 @@ public class CatService {
   }
 
   public CatDetailResDto getByDisplayId(String id) {
-    return catRepository.getByDisplayId(id);
+    return catRepository.getByDisplayId(id)
+        .orElseThrow(() -> new FailedToLoadApiResException(ErrorCode.CAT_IMAGE_NOT_FOUND));
   }
 
 
